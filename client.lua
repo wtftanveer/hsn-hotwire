eeb49c3c669e0745ee2b5d424455b8255bde9ba4
NightOwlsRP <85649796+nightowlsrp@users.noreply.github.com>
2021-08-10 14:07:14 -0700

    init

---
+++ client.lua
@@ -0,0 +1,220 @@
+ESX = nil
+
+Keys = {}
+PlayerData = {}
+SearchedVeh = {}
+local disableF = false
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        ESX = exports['es_extended']:getSharedObject()
+        Citizen.Wait(1)
+    end
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(10)
+    end
+    PlayerData = ESX.GetPlayerData()
+end)
+
+RegisterNetEvent('hsn-hotwire:client:addKeys')
+AddEventHandler('hsn-hotwire:client:addKeys', function(data)
+    Keys[data] = true
+end)
+
+RegisterNetEvent('hsn-hotwire:client:removeKeys')
+AddEventHandler('hsn-hotwire:client:removeKeys',function(plate)
+    Keys[plate] = nil
+end)
+
+function DrawText3Ds(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 370
+	DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+	ClearDrawOrigin()
+end
+
+Citizen.CreateThread(function()
+    while true do
+        local wait = 1000
+        if IsPedInAnyVehicle(PlayerPedId(),false)  then
+            local vehicle = GetVehiclePedIsIn(PlayerPedId())
+            local Plate = GetVehicleNumberPlateText(vehicle)
+            local vehicleCoords = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, 1.25, 0.35)
+            if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), false), -1) == PlayerPedId() then
+                if Keys[Plate] ~= true then
+                    wait = 2
+                    if SearchedVeh[Plate] ~= true then
+                        text = '~y~[H]~s~ Straight Contact | ~g~[Z]~s~ Search'
+                    else
+                        text = '~y~[H]~s~ Straight Contact'
+                    end
+                    if IsControlJustPressed(1, 74) then--H
+                        ESX.TriggerServerCallback('hsn-hotwire:lockpick', function(data)
+                        TriggerServerEvent('hsn-hotwire:lockpick')
+						exports.rprogress:MiniGame({
+							Async = true,
+							Difficulty = "Easy",
+							Easing = "easeLinear",
+							Color = "rgba(255, 255, 255, 1.0)",
+							BGColor = "rgba(0, 0, 0, 0.4)",
+							DisableControls = {
+								Mouse = true,
+								Player = true,
+								Vehicle = true,
+							}, 
+							onComplete = function(success)
+								if success then
+									TriggerServerEvent('hsn-hotwire:addKeys',Plate)
+									SetVehicleEngineOn(vehicle,true)
+									exports['mythic_notify']:SendAlert('inform', 'You have successfully started the vehicle!')
+								else
+									exports['mythic_notify']:SendAlert('inform', 'You failed to make straight contacts!')
+								end    
+							end
+						})
+                        end, "lockpick")
+                    end
+                    if IsControlJustPressed(1, 20) then --Z
+                        if SearchedVeh[Plate] ~= true then
+                            SearchVehicle(Plate)
+                        end
+                    end
+                    DrawText3Ds(vehicleCoords.x,vehicleCoords.y,vehicleCoords.z,text)
+                end
+            end
+        end
+        Citizen.Wait(wait)  
+    end
+end)
+
+RegisterCommand('switch', function()
+    local vehicle = ESX.Game.GetVehicleInDirection()
+    local Plate = GetVehicleNumberPlateText(vehicle)
+    local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+    if vehicle ~= nil then
+        if Keys[Plate] == true then
+            if closestPlayer ~= -1 and closestDistance <= 3.0 then
+                TriggerServerEvent('hsn-hotwire:server:giveKeys',GetPlayerServerId(closestPlayer), Plate)
+            else
+                exports['mythic_notify']:SendAlert('inform', 'There is no one nearby!')
+            end
+        else
+            exports['mythic_notify']:SendAlert('inform', 'You do not have the keys to this vehicle!')
+        end
+    else
+        exports['mythic_notify']:SendAlert('inform', 'You have to look towards the vehicle!')
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local wait = 750
+        local veh = GetVehiclePedIsIn(PlayerPedId() , false)
+        local Plate = GetVehicleNumberPlateText(veh)
+        if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), false), -1) == PlayerPedId() and Keys[Plate] ~= true then
+            wait = 6
+            SetVehicleEngineOn(veh, false)
+        end
+        Citizen.Wait(wait)
+    end
+end)
+
+SearchVehicle = function(plate)
+    SearchedVeh[plate] = true
+	exports.rprogress:Custom({
+		Async = true,
+		Duration = 12500,
+		Easing = "easeLinear",
+		Label = "Searching Vehicle...",
+		DisableControls = {
+			Mouse = true,
+			Player = true,
+			Vehicle = true,
+		},    
+    onComplete = function()
+        TriggerServerEvent('hsn-hotwire:server:SearchVeh', plate)
+    end
+})
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(7)
+        if IsControlJustPressed(1,182) then
+            local coords = GetEntityCoords(PlayerPedId())
+            vehicle = ESX.Game.GetClosestVehicle()
+            local Plate = GetVehicleNumberPlateText(vehicle)
+            if Keys[Plate] == true then
+                local lock = GetVehicleDoorLockStatus(vehicle)
+                if lock == 1 or lock == 0 then
+                    TaskPlayAnim(PlayerPedId(), "anim@mp_player_intmenu@key_fob@", "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
+                    SetVehicleDoorShut(vehicle, 0, false)
+                    SetVehicleDoorShut(vehicle, 1, false)
+                    SetVehicleDoorShut(vehicle, 2, false)
+                    SetVehicleDoorShut(vehicle, 3, false)
+                    SetVehicleDoorsLocked(vehicle, 2)
+                    PlayVehicleDoorCloseSound(vehicle, 1)
+                    SetVehicleLights(vehicle, 2)
+                    SetVehicleLights(vehicle, 0)
+                    SetVehicleLights(vehicle, 2)
+                    SetVehicleLights(vehicle, 0)
+                    exports['mythic_notify']:SendAlert('inform', 'The vehicle is locked.')
+                elseif lock == 2 then
+                    TaskPlayAnim(PlayerPedId(), "anim@mp_player_intmenu@key_fob@", "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
+                    SetVehicleDoorsLocked(vehicle, 1)
+					PlayVehicleDoorOpenSound(vehicle, 0)
+					SetVehicleLights(vehicle, 2)
+					SetVehicleLights(vehicle, 0)
+					SetVehicleLights(vehicle, 2)
+                    SetVehicleLights(vehicle, 0)
+                    exports['mythic_notify']:SendAlert('inform', 'The vehicle is unlocked.')
+                end
+            end
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local wait = 1250
+        if GetVehiclePedIsTryingToEnter(PlayerPedId()) ~= 0 then 
+            local curveh = GetVehiclePedIsTryingToEnter(PlayerPedId())
+            local pedDriver = GetPedInVehicleSeat(curveh, -1)
+            local plate = GetVehicleNumberPlateText(curveh)
+            if Keys[plate] ~= PlayerData.identifier and DoesEntityExist(pedDriver) and IsEntityDead(pedDriver) and not IsPedAPlayer(pedDriver)  then
+                wait = 1
+				exports.rprogress:Custom({
+					Async = true,
+					Duration = 2000,
+					Easing = "easeLinear",
+					Label = "Getting Key...",
+					DisableControls = {
+						Mouse = true,
+						Player = true,
+						Vehicle = true,
+					},    
+				onComplete = function()
+					TriggerServerEvent('hsn-hotwire:addKeys',plate)
+					exports['mythic_notify']:SendAlert('inform', 'You got the car keys!')
+				end
+				})
+            end
+        end
+        Citizen.Wait(wait)
+    end
+end)
+
+AddKeys = function(plate)
+    if plate ~= nil then
+        TriggerServerEvent('hsn-hotwire:addKeys',plate)
+    end
+end
---
+++ server.lua
@@ -0,0 +1,67 @@
+SearchedVehicles = {}
+
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
+end)
+
+RegisterServerEvent('hsn-hotwire:addKeys')
+AddEventHandler('hsn-hotwire:addKeys',function(plate)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    TriggerClientEvent('hsn-hotwire:client:addKeys',src,plate)
+end)
+
+RegisterServerEvent('hsn-hotwire:server:giveKeys')
+AddEventHandler('hsn-hotwire:server:giveKeys',function(target,plate)
+    local src = source
+    local tPlayer = ESX.GetPlayerFromId(target)
+    TriggerClientEvent('hsn-hotwire:client:removeKeys',src,plate)
+    TriggerClientEvent('hsn-hotwire:client:addKeys',tPlayer.source,plate)
+end)
+
+RegisterServerEvent('hsn-hotwire:server:SearchVeh')
+AddEventHandler('hsn-hotwire:server:SearchVeh',function(plate)
+    local src = source 
+    --local Items = ESX.GetItems()
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if SearchedVehicles[plate] ~= true then
+        SearchedVehicles[plate] = true
+        local luck = math.random(1, 4)
+        local money = math.random(20, 200)
+        if luck == 1 then
+            xPlayer.addInventoryItem('cigarette', 1)
+            TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'inform', text = 'You found a Cigarette in the vehicle.'} )
+        elseif luck == 2 then
+            xPlayer.addInventoryItem('water', 1)
+            TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'inform', text = 'You found a water bottle in the vehicle.'} )
+        elseif luck == 3 then
+            xPlayer.addInventoryItem('WEAPON_SNSPISTOL', 1)
+            TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'inform', text = 'You found 1 pistol in the vehicle.'} )
+        elseif luck == 4 then
+            xPlayer.addMoney(money)
+            TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'inform', text = 'in the vehicle '..money..'$ you found it.'} )
+        end
+    else
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'inform', text = plate..' The glove box of the vehicle has already been searched'} )
+    end
+end)
+
+RegisterServerEvent('hsn-hotwire:lockpick')
+AddEventHandler('hsn-hotwire:lockpick', function()
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    xPlayer.removeInventoryItem('lockpick', 1)
+end)
+
+ESX.RegisterServerCallback('hsn-hotwire:lockpick', function(source, cb, item)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	itemcount = xPlayer.getInventoryItem(item).count
+	if itemcount > 0 then
+		cb(true)
+	else
+        activity = 0
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You do not have a lockpick!'} )
+	end
+end)
